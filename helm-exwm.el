0c2ff53f02392d80c4436a056889affbd8bb8e3c
Pierre Neidhardt <ambrevar@gmail.com>
2017-11-09 09:40:05 +0100

    Update documentation for a stable release

--- helm-exwm.el
+++ helm-exwm.el
@@ -22,15 +22,19 @@
 ;; see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;; Helm for EXWM buffers.
+;; `helm-exwm' run a Helm session over the list of EXWM buffers.
 ;;
 ;; To separate EXWM buffers from Emacs buffers in `helm-mini', set up the sources as follows:
 ;;
-;; (setq helm-exwm-emacs-buffers-source (helm-exwm-build-emacs-buffers-source))
-;; (setq helm-exwm-source (helm-exwm-build-source))
-;; (setq helm-mini-default-sources `(helm-exwm-emacs-buffers-source
-;;                                   helm-exwm-source
-;;                                   helm-source-recentf)
+;;   (setq helm-exwm-emacs-buffers-source (helm-exwm-build-emacs-buffers-source))
+;;   (setq helm-exwm-source (helm-exwm-build-source))
+;;   (setq helm-mini-default-sources `(helm-exwm-emacs-buffers-source
+;;                                     helm-exwm-source
+;;                                     helm-source-recentf)
+;;
+;; `helm-exwm-switch' is a convenience X application launcher using Helm to
+;; switch between the various windows of one or several specific applications.
+;; See `helm-exwm-switch-browser' for an example.
 
 ;;; Bugs:
 ;; TODO: kill-persistent is not persistent.
@@ -43,7 +47,6 @@
 ;; count manually.
 
 ;;; Code:
-
 ;; TODO: Post on EXWM's wiki once on MELPA.
 
 (defvar helm-exwm-buffer-max-length 51
